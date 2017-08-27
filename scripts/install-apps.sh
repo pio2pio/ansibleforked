ab01aaef0fbf873bfc2fa35247cbe0d3e005d1af
Steve Stevens <steve.h.i.stevens@gmail.com>
2017-08-27 12:57:29 +0100

    Java7 is no longer an available repo

--- scripts/install-apps.sh
+++ scripts/install-apps.sh
@@ -2,8 +2,10 @@
 
 printf "****************\nInstalling Applications\n****************\n";
 sudo apt-get -y install vim
+sudo apt-get -y install gradle
 sudo apt-get -y install tree
 sudo apt-get -y install vlc
 sudo apt-get -y install xclip
 sudo apt-get -y install baobab
+sudo apt-get -y install libc6-i386 ; # 64bit ubuntu is missing some 32 libraries, this resolves
 
--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 printf "****************\nInstalling Java\n****************\n";
-sudo apt-get -y install oracle-java7-installer
 sudo apt-get -y install oracle-java8-installer
 
