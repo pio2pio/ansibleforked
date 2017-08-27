18750b9fd91c04ac53cbc189f29834a3527aed7b
Steve Stevens <steve.h.i.stevens@gmail.com>
2017-08-27 13:12:55 +0100

    Added some additional 32bit libraries that some applications seem to require

--- scripts/install-apps.sh
+++ scripts/install-apps.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-printf "****************\nInstalling Applications\n****************\n";
+printf "****************\nInstalling 32bit libraries\n****************\n"
+# 64bit ubuntu is missing some 32 libraries, this resolves
+sudo apt-get -y install libc6-i386 
+sudo apt-get -y install lib32z1
+sudo apt-get -y install lib32ncurses5
+sudo apt-get -y install lib32stdc++6
+
+printf "****************\nInstalling Applications\n****************\n"
 sudo apt-get -y install vim
 sudo apt-get -y install gradle
 sudo apt-get -y install tree
 sudo apt-get -y install vlc
 sudo apt-get -y install xclip
 sudo apt-get -y install baobab
-sudo apt-get -y install libc6-i386 ; # 64bit ubuntu is missing some 32 libraries, this resolves
-
