58244d76a53e65f8d6cdc12bf1d49bcfc3a4c0a8
Steve Stevens <steve.h.i.stevens@gmail.com>
2017-08-27 19:27:47 +0100

    powerline fonts are now cleaner

--- scripts/install-python-apps.sh
+++ scripts/install-python-apps.sh
@@ -2,7 +2,11 @@
 
 printf "****************\nInstalling Python libraries\n****************\n"
 pip3 install --user powerline-status
+
 wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
 sudo mv PowerlineSymbols.otf /usr/local/share/fonts/
 sudo fc-cache -f -v
+wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+sudo mkdir -p ~/.config/fontconfig/fonts.conf
+sudo mv 10-powerline-symbols.conf ~/.config/fontconfig/fonts.conf/
 
