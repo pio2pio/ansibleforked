7a194a98f6dcf8ef0b3ec8c7cf90dd2a9c651910
OurFriendIrony <OurFriendIrony@users.noreply.github.com>
2017-09-10 11:55:42 +0100

    install-configs.sh - dirs corrected

--- scripts/install-configs.sh
+++ scripts/install-configs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 printf "****************\nInstalling Config\n****************\n";
-sudo cp ../config/gitconfig ~/.gitconfig
-sudo cp ../config/vimrc ~/.vimrc
-sudo cp ../config/bashrc ~/.bashrc
-sudo cp ../config/bash_functions ~/.bash_functions
-sudo cp ../config/bash_aliases ~/.bash_aliases
+sudo cp ./config/gitconfig ~/.gitconfig
+sudo cp ./config/vimrc ~/.vimrc
+sudo cp ./config/bashrc ~/.bashrc
+sudo cp ./config/bash_functions ~/.bash_functions
+sudo cp ./config/bash_aliases ~/.bash_aliases
 
 # System mem/cpu indicator
-sudo cp ../config/indicator-sysmonitor.json ~/.indicator-sysmonitor.json
+sudo cp ./config/indicator-sysmonitor.json ~/.indicator-sysmonitor.json
 sudo mkdir -p ~/.config/autostart
-sudo cp ../config/autostart/indicator-sysmonitor.desktop ~/.config/autostart/
+sudo cp ./config/autostart/indicator-sysmonitor.desktop ~/.config/autostart/
 
 # Terminator
 sudo mkdir -p ~/.config/terminator
-sudo cp ../config/terminator ~/.config/terminator/config
+sudo cp ./config/terminator ~/.config/terminator/config
