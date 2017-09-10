22d7d4dca414754a0a787b2004fba6fb7b56500c
Steve Stevens <steve.h.i.stevens@gmail.com>
2017-09-10 12:57:17 +0100

    Implement 'vim-airline' instead of powerline, using Vundle

--- scripts/install-configs.sh
+++ scripts/install-configs.sh
@@ -2,7 +2,7 @@
 
 printf "****************\nInstalling Config\n****************\n";
 sudo cp ./config/gitconfig ~/.gitconfig
-sudo cp ./config/vimrc ~/.vimrc
+# sudo cp ./config/vimrc ~/.vimrc                          [~/scripts/install-apps-vim.sh]
 sudo cp ./config/bashrc ~/.bashrc
 sudo cp ./config/bash_functions ~/.bash_functions
 sudo cp ./config/bash_aliases ~/.bash_aliases
--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 ./scripts/install-sys-update.sh
 ./scripts/install-java.sh
-./scripts/install-apps.sh
+./scripts/install-apps-general.sh
 ./scripts/install-apps-gradle.sh
-./scripts/install-apps-powerline.sh
+./scripts/install-apps-vim.sh
 ./scripts/install-configs.sh
 ./scripts/env-clean.sh
 ./scripts/notes.sh
