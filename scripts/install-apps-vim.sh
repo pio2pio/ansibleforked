8566804e6664b2d8d41ade3fb57b5b5032ec9251
Steve Stevens <steve.h.i.stevens@gmail.com>
2017-10-01 21:59:28 +0000

    vim config installation a bit smarter

--- scripts/install-apps-vim.sh
+++ scripts/install-apps-vim.sh
@@ -5,20 +5,23 @@ printf "****************\nInstalling Vim and Vundle\n****************\n"
 sudo apt-get -y install vim
 
 # Setup Vundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+     git clone https://github.com/gmarik/vundle.git      ~/.vim/bundle/vundle
+sudo git clone https://github.com/gmarik/vundle.git      /root/.vim/bundle/vundle
 
 # Config
-cp ./config/vimrc ~/.vimrc
-sudo cp ./config/vimrc /root/.vimrc
+     cp ./cfg/.vimrc                                     ~            # [vim]
+sudo cp ./cfg/.vimrc                                     /root        # [vim]
 
 # Setup powerline fonts
 sudo mkdir -p /usr/local/share/fonts
-mkdir -p ~/.config/fontconfig/fonts.conf
-sudo wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf -P /usr/local/share/fonts/
-wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf -P ~/.config/fontconfig/fonts.conf/
+     mkdir -p ~/.config/fontconfig/fonts.conf
+
+sudo wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf      -P /usr/local/share/fonts/
+     wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf -P ~/.config/fontconfig/fonts.conf/
+
 sudo fc-cache -fv
 
 # Install Vundle plugins
-vim +PluginInstall +qall
+     vim +PluginInstall +qall
 sudo vim +PluginInstall +qall
 
