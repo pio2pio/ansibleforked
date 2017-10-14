ca1e86d1907d47e57e43aff15c32c2e7e11aa971
Steve Stevens <steve.h.i.stevens@gmail.com>
2017-10-14 01:08:48 +0100

    Start to ansiblise all my scripts

---
+++ go.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+path_config="$(pwd)/ansible-sys/ansible.cfg"
+path_hosts="$(pwd)/ansible-sys/hosts"
+path_roles="$(pwd)/ansible-sys/roles/"
+
+sed -i 's:\(inventory.*=\).*:\1 '"${path_hosts}"':g' ${path_config}
+sed -i 's:\(roles_path.*=\).*:\1 '"${path_roles}"':g' ${path_config}
+
+export ANSIBLE_CONFIG="${path_config}"
+
+#######################################################################
+
+ansible-playbook build.yml
+
