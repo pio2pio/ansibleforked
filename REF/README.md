e343c318d7dddc27a92f783407742d3f6639851c
Steve Stevens <steve.h.i.stevens@gmail.com>
2017-10-22 16:27:14 +0100

    Adds bash

--- REF/README.md
+++
@@ -1,6 +0,0 @@
-## Pull
-`git clone --recursive git@github.com:OurFriendIrony/ubuntu-setup.git`
-
-## Update
-`git submodule update --recursive --remote`
-
--- REF/scripts/install-apps-general.sh
+++ REF/scripts/install-apps-general.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 printf "****************\nInstalling APT Applications\n****************\n"
-sudo apt-get -y install tree
-sudo apt-get -y install vlc
-sudo apt-get -y install xclip
-sudo apt-get -y install baobab
-sudo apt-get -y install ruby
 sudo apt-get -y install terminator
 sudo apt-get -y install keychain
 
