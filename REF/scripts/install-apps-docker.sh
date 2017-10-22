81617424032ff572577e7f5f3228cdec28c5dc4d
Steve Stevens <steve.h.i.stevens@gmail.com>
2017-10-23 00:23:19 +0100

    Docker

--- REF/scripts/install-apps-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo apt-get -y install docker-ce
-sudo chmod 666 /var/run/docker.sock
-
