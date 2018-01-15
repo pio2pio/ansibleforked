5774996af6467e5ec8c1d105e29b39c078471622
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-01-15 21:04:49 +0000

    tweak the wallpaper service

--- roles/scripts/add_launcher.sh
+++ roles/scripts/add_launcher.sh
@@ -1,3 +1,3 @@
-application="\'application:\/\/${1}.desktop\'"
+application="\'${1}.desktop\'"
 gsettings set com.canonical.Unity.Launcher favorites "$(gsettings get com.canonical.Unity.Launcher favorites | sed "s/, *${application} *//g" | sed "s/${application} *, *//g" | sed -e "s/]$/, ${application}]/")"
 
